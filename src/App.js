df71035e864f1a93d993d3bbdec65429753ef18d
cypowers <cypark73@hotmail.com>
2025-07-28 15:24:42 +0900

    P358

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
-import TestComp from './component/TestComp';
-import { useState, useRef } from 'react';
+import { useReducer, useRef } from 'react';
 
 const mockTodo = [
   { id: 0, isDone: false, content: 'React 공부하기', createDate: new Date().getTime() },
@@ -12,36 +11,55 @@ const mockTodo = [
   
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.newItem, ...state];
+    case 'UPDATE':
+      return state.map(item => 
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case 'DELETE':
+      return state.filter(item => item.id !== action.targetId);
+    default:
+      return state;
+  }
+  
+}
+
 function App() {
-  const [todo, setTodo] = useState(mockTodo);
+  // const [todo, setTodo] = useState(mockTodo);
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newItem = {
-      id: idRef.current,
-      isDone: false,
-      content,
-      createDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
+    dispatch({ type: 'CREATE',
+               newItem: {
+                  id: idRef.current,
+                  isDone: false,
+                  content: content,
+                  createDate: new Date().getTime()
+               }
+     });
     idRef.current += 1;
   };
 
   const onUpdate = (targetId) => {
-    setTodo(
-      todo.map(item => 
-        item.id === targetId ? { ...item, isDone: !item.isDone }
-        : item
-    ));
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId
+    });
   };
 
   const onDelete = (targetId) => {
-    setTodo(todo.filter(item => item.id !== targetId));
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId
+    });
   };
 
   return (
     <div className="App">
-      <TestComp />
       <Header />
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete}/>
